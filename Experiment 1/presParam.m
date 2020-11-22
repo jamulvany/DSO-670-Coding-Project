73262b8ada77a78667f35f073c09861e2c3b0a78
jamulvany <74766401+jamulvany@users.noreply.github.com>
2020-11-22 14:28:10 -0800

    Add files via upload

---
+++ Experiment 1/presParam.m
@@ -0,0 +1,52 @@
+%Finds the price given by the prescriptive parametric method given the
+%number of data points and the coefficient c in Z = c*X+W
+
+function [price,revenue] = presParam(n,confound)
+
+%Define demand model
+tau = sqrt(15.1234);
+X = normrnd(0,1,n,1);
+V = normrnd(0,1,n,1);
+W = normrnd(0,tau,n,1);
+Z = confound*X + W;
+Y = 27.75 - Z.^2 + 6*X.*Z - 9*X.^2 + V;
+
+%Regress Z on X
+b = regress(Z, [ones(n,1) X]);
+
+%Find estimates for Z and c given coefficients from regression
+ZHat = b(1) + X*b(2);
+tauHat = sqrt(sum((Z-ZHat).^2)/n);
+
+%Impute propensity score corresponding to each data point
+QHat = ( 1/(sqrt(2*pi)*tauHat) )  * exp ( -1/(2*tauHat^2)*(Z-ZHat).^2 );
+
+%Regress Y on Z,Z^2 and log(QHat)
+Data = [ones(n,1) Z Z.^2 log(QHat)];
+c = regress(Y, Data);
+
+%Given estimate for y(z) yHat(z), optimize Rhat(z)=z*yHat(z) using 1-D grid search
+z = linspace(0,5,1000);
+optPresIndex = -1;
+optPresObj = -100;
+
+for i = 1:length(z)
+
+    yHat = c(1) + c(2)*z(i) + c(3)*z(i)^2  + c(4)*log( ( 1/(sqrt(2*pi)*tauHat) )  * exp ( -1/(2*tauHat^2)*(z(i)-ZHat).^2 ) );
+
+    newObj = z(i)*mean(yHat);
+    
+    if ( newObj > optPresObj )
+        
+        optPresObj = newObj;
+        optPresIndex = i;
+        
+    end
+end
+
+%return the price and out of sample revenue
+price = z(optPresIndex);
+revenue = price*(18.75 - price^2);
+
+end
+
